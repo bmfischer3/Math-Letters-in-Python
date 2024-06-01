fa68a132fc4e380a47eeb597477a207c586a4493
bmfischer3 <30556905+bmfischer3@users.noreply.github.com>
2024-06-01 07:37:06 -0400

    first commit - committing all files to the repository

---
+++ Chapter-0_Basic-Mathetical-Concepts.py
@@ -0,0 +1,4 @@
+# Chapter 0 - Basic Mathematical Concepts
+
+class Math:
+    def 
---
+++ Chapter-0_The-Collatz-Conjecture.py
@@ -0,0 +1,5 @@
+# Chapter 1 - The Colltaz Conjecture
+
+class Collatz:
+    def __init__(self) -> None:
+        pass
